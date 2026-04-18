1523bfe289008661e7c908b185c806d4939c1adc
Your Name <you@example.com>
2026-04-18 01:59:43 -0300

    Set up ejb-module and add Beneficio entity

---
+++ ejb-module/src/main/java/com/example/ejb/Beneficio.java
@@ -0,0 +1,89 @@
+package com.example.ejb;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Version;
+
+@Entity
+public class Beneficio implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	private String descricao;
+	private BigDecimal valor;
+	private Boolean ativo;
+	
+	@Version
+	private Long version;
+	
+	public Beneficio() {
+		
+	}
+
+	public Beneficio(Long id, String nome, String descricao, BigDecimal valor, Boolean ativo, Long version) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.descricao = descricao;
+		this.valor = valor;
+		this.ativo = ativo;
+		this.version = version;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public BigDecimal getValor() {
+		return valor;
+	}
+
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+
+	public Boolean getAtivo() {
+		return ativo;
+	}
+
+	public void setAtivo(Boolean ativa) {
+		this.ativo = ativa;
+	}
+
+	public Long getVersion() {
+		return version;
+	}
+
+	public void setVersion(Long version) {
+		this.version = version;
+	}
+}
